89d676bb0c5c1d09bb67260db308e22713aa74ab
Zack <dellaroccozack@gmail.com>
2020-08-10 02:00:49 -0400

    Help Command Added

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,11 @@
-exports.run = (client, msg, args) => {
+exports.run = (client, msg, args, Discord) => {
     msg.react("📬")
 
-    msg.author.send("**Help command is a work in progress!**")
+    helpEmbed = new Discord.MessageEmbed()
+    .setTitle("Help")
+    .setColor("#03a5fc")
+    .addField("**Help command is a work in progress!**", "*Please come back when it's finished!*")
+    .setFooter("Made with ❤ by ImOsiris#2222");
+
+    msg.author.send(helpEmbed)
 }
