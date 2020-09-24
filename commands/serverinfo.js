69663b190eb70356943db7cb29bebf6992f5758b
Zack <dellaroccozack@gmail.com>
2020-09-24 14:02:48 -0400

    Serverinfo command

---
+++ commands/serverinfo.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+
+exports.run = (client, msg, args) => {
+    const serverEmbed = new Discord.MessageEmbed();
+    serverEmbed.setColor("#0082ff");
+    serverEmbed.setTitle("Server Info");
+    serverEmbed.setThumbnail(msg.guild.iconURL());
+    serverEmbed.setDescription(`**${msg.guild}**'s information`);
+    serverEmbed.addField("Owner", `The owner of this server is ${msg.guild.owner}`);
+    serverEmbed.addField("Member Count", `This server has ${msg.guild.memberCount} members`);
+    serverEmbed.addField("Emoji Count", `This server has ${msg.guild.emojis.cache.size} emojis`);
+    serverEmbed.addField("Roles Count", `This server has ${msg.guild.roles.cache.size} roles.`);
+
+    msg.channel.send(serverEmbed)
+}
